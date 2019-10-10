c8a0b7f300a76a09f7c48d7ba77a8974537ffc8b
patrick.lake2 <52745289+patricklake2@users.noreply.github.com>
2019-10-10 16:34:30 +0100

    corrected 2050 column rounding

--- data/scenarios/primaries/round.py
+++ data/scenarios/primaries/round.py
@@ -4,7 +4,7 @@ import os
 for filename in os.listdir():
     if filename.startswith('EV'):
         df = pd.read_csv(filename)
-        for year in range(2017,2050):
+        for year in range(2017,2051):
             df[str(year)] = df.apply(lambda row: round(row[str(year)]), axis=1)
         os.remove(filename)
         df.to_csv(filename, index=False)
