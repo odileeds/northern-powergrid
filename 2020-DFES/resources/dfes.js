0df29fbf8e83bdc09ef326090080e95764e41232
slowe <gravatar@strudel.org.uk>
2020-11-26 14:44:56 +0000

    Add colour scale and fix NaNs in CSV

--- 2020-DFES/resources/dfes.js
+++ 2020-DFES/resources/dfes.js
@@ -13,7 +13,7 @@
 	// Main function
 	function FES(config){
 
-		this.version = "1.2.3";
+		this.version = "1.2.4";
 		if(!config) config = {};
 		this.options = (config.options||{});
 		this.parameters = {};
@@ -23,7 +23,6 @@
 		this.views = (config.views||{});
 		this.events = {};
 		if(config.on) this.events = config.on;
-		
 		
 		S().ajax(path+"data/scenarios/config.json",{
 			'this':this,
@@ -82,7 +81,6 @@
 		}
 		if(this.parameters && S('#parameters').length==0){
 			var html = "";
-			if(!this.data.scenarios[this.options.scenario]) this.message('Scenario <em>"'+this.options.scenario+'"</em> is not defined in index.json.',{'id':'scenario','type':'ERROR'});
 			var css = this.data.scenarios[this.options.scenario].css;
 			for(var p in this.parameters) html += "<option"+(this.options.parameter == p ? " selected=\"selected\"":"")+" value=\""+p+"\">"+this.parameters[p].title+"</option>";
 			S('#parameter-holder').html('<select id="parameters">'+html+'</select><div class="about"></div>');
@@ -324,8 +322,8 @@
 				for(c = 0; c < data.raw.fields.name.length; c++){
 					if(parseInt(data.raw.fields.name[c])==data.raw.fields.name[c]){
 						for(r = 0; r < data.raw.rows.length; r++){
-							// Convert to numbers
-							data.raw.rows[r][c] = parseFloat(data.raw.rows[r][c]);
+							// Convert to numbers - if the number doesn't parse replace with zero
+							data.raw.rows[r][c] = (parseFloat(data.raw.rows[r][c])||0);
 						}
 					}
 				}
@@ -437,7 +435,6 @@
 												}
 
 											}else{
-
 												// If this layer uses the current source as "data" we can set it
 												data.layers[l].values[pkey][d.raw.fields.name[c]] = (typeof v==="number") ? v : d.raw.rows[r][c];
 											}
@@ -626,7 +623,6 @@
 						layer.setStyle({
 							weight: 2,
 							color: color,
-							fillColor: color,
 							opacity: 1,
 							stroke: true
 						});
@@ -674,13 +670,27 @@
 						
 						// Get a nicer range
 						this.views[this.options.view].layers[l].range = niceRange(this.views[this.options.view].layers[l].range.min,this.views[this.options.view].layers[l].range.max);
+						if(!this.views[this.options.view].layers[l].colour){
+							this.views[this.options.view].layers[l].colour = new Colours();
+						}
+						// Add/update a continuous colour scale
+						this.views[this.options.view].layers[l].colourscale = 'DFES-continuous';
+						this.views[this.options.view].layers[l].colour.addScale(this.views[this.options.view].layers[l].colourscale,getRGBAstr(color,0.0)+' 0%, '+getRGBAstr(color,0.8)+' 100%');
+						if(typeof this.options.map.quantised==="number"){
+							// Add/update a quantised colour scale
+							this.views[this.options.view].layers[l].colour.quantiseScale(this.views[this.options.view].layers[l].colourscale,this.options.map.quantised,'DFES-quantised');
+							this.views[this.options.view].layers[l].colourscale = 'DFES-quantised';
+						}
+							
 						// Update the scale bar
-						S('#scale').html(makeScaleBar(getRGBAstr(color,0.0),getRGBAstr(color,0.8),{
+						S('#scale').html(makeScaleBar(this.views[this.options.view].layers[l].colour.getGradient( this.views[this.options.view].layers[l].colourscale ),{
 							'min': this.views[this.options.view].layers[l].range.min,
 							'max': this.views[this.options.view].layers[l].range.max,
-							'weight': this.views[this.options.view].layers[l].geoattr.style.weight,
-							'color': this.views[this.options.view].layers[l].geoattr.style.color,
-							'units': this.parameters[this.options.parameter].units
+							'color': color,
+							'units': this.parameters[this.options.parameter].units,
+							'scale': this.views[this.options.view].layers[l].colour,
+							'scaleid': this.views[this.options.view].layers[l].colourscale,
+							'levels': (typeof this.options.map.quantised==="number" ? this.options.map.quantised : undefined)
 						}));
 						
 						// Define the GeoJSON attributes for this layer
@@ -694,18 +704,18 @@
 							};
 							if(layer.boundary && typeof layer.boundary.stroke==="boolean") props.stroke = layer.boundary.stroke;
 							if(feature.geometry.type == "Polygon" || feature.geometry.type == "MultiPolygon"){
-								var v = 0;
+								var c = {'r':0,'g':0,'b':0,'alpha':0};
 								var data = _scenario[layer.id];
 								var key = _obj.layers[layer.id].key;
 								if(feature.properties[key] && data.values[feature.properties[key]]){
-									v = (data.values[feature.properties[key]][_obj.options.key]-layer.range.min)/(layer.range.max-layer.range.min)
+									c = layer.colour.getColourFromScale( layer.colourscale, data.values[feature.properties[key]][_obj.options.key],layer.range.min,layer.range.max,true);
 								}else{
-									console.warn('Unable to find '+key,feature.properties[key],data)
+									console.warn('Unable to find '+key,feature.properties)
 								}
-								v *= 0.8; // Maximum opacity
+								props.fillColor = 'rgb('+c.r+','+c.g+','+c.b+')';
 								props.weight = (layer.boundary ? layer.boundary.strokeWidth||1 : 1);
 								props.opacity = 0.1;
-								props.fillOpacity = v;
+								props.fillOpacity = c.alpha;
 							}
 							return props;
 						};
@@ -716,7 +726,7 @@
 								'mouseover':highlightFeature,
 								'mouseout': resetHighlight
 							}
-							if(popup) layer.bindPopup(popup);
+							if(popup) layer.bindPopup('<div class="dfes-popup-content"><div class="dfes-popup-inner">'+popup+'</div></div>');
 							layer.on(attr);
 						}
 					}
@@ -1023,18 +1033,24 @@
 		return lastIndexOf;
 	}
 
-	function makeScaleBar(a,b,attr){
+	function makeScaleBar(grad,attr){
 		if(!attr) attr = {};
 		if(!attr.min) attr.min = 0;
 		if(!attr.max) attr.max = 0;
 		if(!attr.units) attr.units = "";
 		if(attr.units) attr.units = "&thinsp;"+attr.units;
-		return '<div class="bar" style="'+makeGradient(a,b)+';border:'+attr.weight+'px solid '+attr.color+'"></div><div class="range"><span class="min" style="border-left:'+attr.weight+'px solid '+attr.color+'">'+attr.min.toLocaleString()+attr.units+'</span><span class="max" style="border-right:'+attr.weight+'px solid '+attr.color+'">'+attr.max.toLocaleString()+attr.units+'</span></div>';
-	}
-
-	function makeGradient(a,b){
-		if(!b) b = a;
-		return 'background: '+a+'; background: -moz-linear-gradient(left, '+a+' 0%, '+b+' 100%);background: -webkit-linear-gradient(left, '+a+' 0%,'+b+' 100%);background: linear-gradient(to right, '+a+' 0%,'+b+' 100%);';
+		var str = '<div class="bar" style="'+grad+';"><dic class="bar-inner" style="border-color: '+attr.color+'"></div></div><div class="range" style="border-color: '+attr.color+'">';
+		if(attr.levels){
+			var gap,i,v;
+			gap = (attr.max-attr.min)/attr.levels;
+			for(i = 0; i <= attr.levels; i++){
+				v = attr.min + i*gap;
+				c = attr.scale.getColourFromScale(attr.scaleid, v, attr.min, attr.max);
+				str += '<span class="lvl'+(i==0 ? ' min' : (i==attr.levels ? ' max':''))+'" style="border-color: '+(i==0 ? attr.color : c)+';left:'+(100*i/attr.levels)+'%;">'+v.toLocaleString()+attr.units+'</span>'
+			}
+		}
+		str += '</div>';
+		return str;
 	}
 
 	function getRGBAstr(c,a){
@@ -1084,5 +1100,198 @@
 	}
 
 	root.FES = function(config){ return new FES(config); };
+	
+
+	/* ============== */
+	/* Colours v0.3.2 */
+	// Define colour routines
+	function Colour(c,n){
+		if(!c) return {};
+		function d2h(d) { return ((d < 16) ? "0" : "")+d.toString(16);}
+		function h2d(h) {return parseInt(h,16);}
+		/**
+		 * Converts an RGB color value to HSV. Conversion formula
+		 * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+		 * Assumes r, g, and b are contained in the set [0, 255] and
+		 * returns h, s, and v in the set [0, 1].
+		 *
+		 * @param	Number  r		 The red color value
+		 * @param	Number  g		 The green color value
+		 * @param	Number  b		 The blue color value
+		 * @return  Array			  The HSV representation
+		 */
+		function rgb2hsv(r, g, b){
+			r = r/255;
+			g = g/255;
+			b = b/255;
+			var max = Math.max(r, g, b), min = Math.min(r, g, b);
+			var h, s, v = max;
+			var d = max - min;
+			s = max == 0 ? 0 : d / max;
+			if(max == min) h = 0; // achromatic
+			else{
+				switch(max){
+					case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+					case g: h = (b - r) / d + 2; break;
+					case b: h = (r - g) / d + 4; break;
+				}
+				h /= 6;
+			}
+			return [h, s, v];
+		}
+
+		this.alpha = 1;
+
+		// Let's deal with a variety of input
+		if(c.indexOf('#')==0){
+			this.hex = c;
+			this.rgb = [h2d(c.substring(1,3)),h2d(c.substring(3,5)),h2d(c.substring(5,7))];
+		}else if(c.indexOf('rgb')==0){
+			var bits = c.match(/[0-9\.]+/g);
+			if(bits.length == 4) this.alpha = parseFloat(bits[3]);
+			this.rgb = [parseInt(bits[0]),parseInt(bits[1]),parseInt(bits[2])];
+			this.hex = "#"+d2h(this.rgb[0])+d2h(this.rgb[1])+d2h(this.rgb[2]);
+		}else return {};
+		this.hsv = rgb2hsv(this.rgb[0],this.rgb[1],this.rgb[2]);
+		this.name = (n || "Name");
+		var r,sat;
+		for(r = 0, sat = 0; r < this.rgb.length ; r++){
+			if(this.rgb[r] > 200) sat++;
+		}
+		this.toString = function(){
+			return 'rgb'+(this.alpha < 1 ? 'a':'')+'('+this.rgb[0]+','+this.rgb[1]+','+this.rgb[2]+(this.alpha < 1 ? ','+this.alpha:'')+')'
+		}
+		this.text = (this.rgb[0]*0.299 + this.rgb[1]*0.587 + this.rgb[2]*0.114 > 186 ? "black":"white");
+		return this;
+	}
+	function Colours(){
+		var scales = {
+			'Viridis': 'rgb(68,1,84) 0%, rgb(72,35,116) 10%, rgb(64,67,135) 20%, rgb(52,94,141) 30%, rgb(41,120,142) 40%, rgb(32,143,140) 50%, rgb(34,167,132) 60%, rgb(66,190,113) 70%, rgb(121,209,81) 80%, rgb(186,222,39) 90%, rgb(253,231,36) 100%'
+		};
+		function col(a){
+			if(typeof a==="string") return new Colour(a);
+			else return a;
+		}
+		this.getColourPercent = function(pc,a,b,inParts){
+			var c;
+			pc /= 100;
+			a = col(a);
+			b = col(b);
+			c = {'r':parseInt(a.rgb[0] + (b.rgb[0]-a.rgb[0])*pc),'g':parseInt(a.rgb[1] + (b.rgb[1]-a.rgb[1])*pc),'b':parseInt(a.rgb[2] + (b.rgb[2]-a.rgb[2])*pc)};
+			if(a.alpha<1 || b.alpha<1) c.alpha = ((b.alpha-a.alpha)*pc + a.alpha);
+			if(inParts) return c;
+			else return 'rgb'+(c.alpha && c.alpha<1 ? 'a':'')+'('+c.r+','+c.g+','+c.b+(c.alpha && c.alpha<1 ? ','+c.alpha:'')+')';
+		};
+		this.makeGradient = function(a,b){
+			a = col(a);
+			b = col(b);
+			var grad = a.toString()+' 0%, '+b.toString()+' 100%';
+			if(b) return 'background: '+a.toString()+'; background: -moz-linear-gradient(left, '+grad+');background: -webkit-linear-gradient(left, '+grad+');background: linear-gradient(to right, '+grad+');';
+			else return 'background: '+a.toString()+';';
+		};
+		this.getGradient = function(id){
+			return 'background: -moz-linear-gradient(left, '+scales[id].str+');background: -webkit-linear-gradient(left, '+scales[id].str+');background: linear-gradient(to right, '+scales[id].str+');';
+		};
+		this.addScale = function(id,str){
+			scales[id] = str;
+			processScale(id,str);
+			return this;
+		}
+		this.quantiseScale = function(id,n,id2){
+			var cs,m,pc,step,i;
+			cs = [];
+			m = n-1;
+			pc = 0;
+			step = 100/n;
+			for(i = 0; i < m; i++){
+				cs.push(this.getColourFromScale(id,i,0,m)+' '+(pc)+'%');
+				cs.push(this.getColourFromScale(id,i,0,m)+' '+(pc+step)+'%');
+				pc += step;
+			}
+			cs.push(this.getColourFromScale(id,1,0,1)+' '+(pc)+'%');
+			cs.push(this.getColourFromScale(id,1,0,1)+' 100%');
+			this.addScale(id2,cs.join(", "));
+			return this;
+		}
+		function processScale(id,str){
+			if(scales[id] && scales[id].str){
+				console.warn('Colour scale '+id+' already exists. Bailing out.');
+				return this;
+			}
+			scales[id] = {'str':str};
+			scales[id].stops = extractColours(str);
+			return this;
+		}
+		function extractColours(str){
+			var stops,cs,i,c;
+			stops = str.replace(/^\s+/g,"").replace(/\s+$/g,"").replace(/\s\s/g," ").split(', ');
+			cs = [];
+			for(i = 0; i < stops.length; i++){
+				var bits = stops[i].split(/ /);
+				if(bits.length==2) cs.push({'v':bits[1],'c':new Colour(bits[0])});
+				else if(bits.length==1) cs.push({'c':new Colour(bits[0])});
+			}
+			
+			for(c=0; c < cs.length;c++){
+				if(cs[c].v){
+					// If a colour-stop has a percentage value provided, 
+					if(cs[c].v.indexOf('%')>=0) cs[c].aspercent = true;
+					cs[c].v = parseFloat(cs[c].v);
+				}
+			}
+			return cs;
+		}
+
+		// Process existing scales
+		for(var id in scales){
+			if(scales[id]) processScale(id,scales[id]);
+		}
+		
+		// Return a Colour object for a string
+		this.getColour = function(str){
+			return new Colour(str);
+		};
+		// Return the colour scale string
+		this.getColourScale = function(id){
+			return scales[id].str;
+		};
+		// Return the colour string for this scale, value and min/max
+		this.getColourFromScale = function(s,v,min,max,inParts){
+			var cs,v2,pc,c,cfinal;
+			var colour = "";
+			if(typeof inParts!=="boolean") inParts = false;
+			if(!scales[s]){
+				console.warn('No colour scale '+s+' exists');
+				return '';
+			}
+			if(typeof min!=="number") min = 0;
+			if(typeof max!=="number") max = 1;
+			cs = scales[s].stops;
+			v2 = 100*(v-min)/(max-min);
+			var match = -1;
+			cfinal = {};
+			if(v==max){
+				cfinal = {'r':cs[cs.length-1].c.rgb[0],'g':cs[cs.length-1].c.rgb[1],'b':cs[cs.length-1].c.rgb[2],'alpha':cs[cs.length-1].c.alpha};
+			}else{
+				if(cs.length == 1){
+					cfinal = {'r':cs[0].c.rgb[0],'g':cs[0].c.rgb[1],'b':cs[0].c.rgb[2],'alpha':(v2/100).toFixed(3)};
+				}else{
+					for(c = 0; c < cs.length-1; c++){
+						if(v2 >= cs[c].v && v2 <= cs[c+1].v){
+							// On this colour stop
+							pc = 100*(v2 - cs[c].v)/(cs[c+1].v-cs[c].v);
+							if(pc > 100) pc = 100;	// Don't go above colour range
+							cfinal = this.getColourPercent(pc,cs[c].c,cs[c+1].c,true);
+							continue;
+						}
+					}
+				}
+			}
+			if(inParts) return cfinal;
+			else return 'rgba(' + cfinal.r + ',' + cfinal.g + ',' + cfinal.b + ',' + cfinal.alpha + ")";
+		};
+		
+		return this;
+	}
 
 })(window || this);
