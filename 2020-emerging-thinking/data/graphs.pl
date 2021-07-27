76d5fc58bb75356a18db1fb9b2744dd9efe002fc
slowe <gravatar@strudel.org.uk>
2021-07-27 13:29:27 +0100

    Correct paths

--- 2020-emerging-thinking/data/graphs.pl
+++ 2020-emerging-thinking/data/graphs.pl
@@ -14,7 +14,6 @@ use POSIX qw(strftime);
 use JSON::XS;
 use ODILeeds::NPG;
 
-
 # Get the scenario config
 open(FILE,$dir."scenarios/index.json");
 @lines = <FILE>;
@@ -52,7 +51,7 @@ $graph->setScenarios(%scenarios);
 
 for($i = 0; $i < (@graphs); $i++){
 	
-	$graph->load('graphs/'.$graphs[$i]{'csv'})->process();
+	$graph->load($dir.'graphs/'.$graphs[$i]{'csv'})->process();
 	
 	# If we have a y-axis scaling we scale the values
 	if($graphs[$i]{'yscale'}){
@@ -60,12 +59,12 @@ for($i = 0; $i < (@graphs); $i++){
 	}
 	
 	# Output the SVG
-	open(FILE,'>','graphs/'.$graphs[$i]{'svg'});
+	open(FILE,'>',$dir.'graphs/'.$graphs[$i]{'svg'});
 	print FILE $graph->draw(('yaxis-label'=>$graphs[$i]{'yaxis-label'},'yscale'=>$graphs[$i]{'yscale'},'yaxis-max'=>$graphs[$i]{'yaxis-max'},'width'=>'640','xaxis-max'=>2051,'xaxis-line'=>1,'stroke'=>3,'strokehover'=>5,'point'=>4,'pointhover'=>6,'line'=>2,'yaxis-format'=>"commify",'yaxis-labels-baseline'=>'middle','xaxis-ticks'=>1,'left'=>$graphs[$i]{'left'}));
 	close(FILE);
 	
 	# Output the HTML table
-	open(FILE,'>','graphs/'.$graphs[$i]{'table'});
+	open(FILE,'>',$dir.'graphs/'.$graphs[$i]{'table'});
 	print FILE $graph->table(());
 	close(FILE);
 }
