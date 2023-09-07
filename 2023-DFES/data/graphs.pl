d6f70b06333487c785ef8d308720f52c17f5a4cb
slowe <299787+slowe@users.noreply.github.com>
2023-09-07 12:37:18 +0100

    Only process if the index.json file exists

--- 2023-DFES/data/graphs.pl
+++ 2023-DFES/data/graphs.pl
@@ -36,56 +36,56 @@ foreach $line  (@lines){
 	}
 }
 
+$indexfile = $dir."graphs/index.json";
+
+if(-e $indexfile){
+	# Get the graph config
+	open(FILE,$indexfile);
+	@lines = <FILE>;
+	close(FILE);
+	@graphs = @{JSON::XS->new->utf8->decode(join("\n",@lines))};
 
 
-# Get the graph config
-open(FILE,$dir."graphs/index.json");
-@lines = <FILE>;
-close(FILE);
-@graphs = @{JSON::XS->new->utf8->decode(join("\n",@lines))};
 
+	# Create the SVG output
+	$graph = ODILeeds::NPG->new();
+	$graph->setScenarios(%scenarios);
 
-
-# Create the SVG output
-$graph = ODILeeds::NPG->new();
-$graph->setScenarios(%scenarios);
-
-$html = "";
-for($i = 0; $i < (@graphs); $i++){
-	print "Processing ".$dir."graphs/$graphs[$i]{'csv'}\n";
-	$graph->load($dir.'graphs/'.$graphs[$i]{'csv'})->process();
-	
-	# If we have a y-axis scaling we scale the values
-	if($graphs[$i]{'yscale'}){
-		$graph->scaleY($graphs[$i]{'yscale'});
+	$html = "";
+	for($i = 0; $i < (@graphs); $i++){
+		print "Processing ".$dir."graphs/$graphs[$i]{'csv'}\n";
+		$graph->load($dir.'graphs/'.$graphs[$i]{'csv'})->process();
+		
+		# If we have a y-axis scaling we scale the values
+		if($graphs[$i]{'yscale'}){
+			$graph->scaleY($graphs[$i]{'yscale'});
+		}
+		
+		# Output the SVG
+		open(FILE,'>',$dir.'graphs/'.$graphs[$i]{'svg'});
+		print FILE $graph->draw(('yaxis-label'=>$graphs[$i]{'yaxis-label'},'yscale'=>$graphs[$i]{'yscale'},'yaxis-max'=>$graphs[$i]{'yaxis-max'},'width'=>'640','xaxis-max'=>2051,'xaxis-line'=>1,'stroke'=>3,'strokehover'=>5,'point'=>4,'pointhover'=>6,'line'=>2,'yaxis-format'=>"commify",'yaxis-labels-baseline'=>'middle','xaxis-ticks'=>1,'left'=>$graphs[$i]{'left'}));
+		close(FILE);
+		
+		# Output the HTML table
+		open(FILE,'>',$dir.'graphs/'.$graphs[$i]{'table'});
+		print FILE $graph->table(());
+		close(FILE);
+		
+		$html .= "\t\t\t<figure class=\"jekyll-parse\">\n";
+		$html .= "\t\t\t\t<figcaption><strong>Figure ".($i+1).":</strong> $graphs[$i]{'title'}</figcaption>\n";
+		$html .= "\t\t\t\t<div class=\"table-holder\">{% include_relative data/graphs/$graphs[$i]{'table'} %}</div>\n";
+		$html .= "\t\t\t\t{% include_relative data/graphs/$graphs[$i]{'svg'} %}\n";
+		$html .= "\t\t\t\t<div class=\"download\">\n";
+		$html .= "\t\t\t\t\t<a href=\"data/graphs/$graphs[$i]{'svg'}\"><img src=\"resources/download.svg\" alt=\"download\" title=\"Download graph from Figure ".($i+1)."\" /> SVG</a>\n";
+		$html .= "\t\t\t\t\t<a href=\"data/graphs/$graphs[$i]{'csv'}\"><img src=\"resources/download.svg\" alt=\"download\" title=\"Download data from Figure ".($i+1)."\" /> CSV</a>\n";
+		$html .= "\t\t\t\t</div>\n";
+		$html .= "\t\t\t</figure>\n\n";
 	}
-	
-	# Output the SVG
-	open(FILE,'>',$dir.'graphs/'.$graphs[$i]{'svg'});
-	print FILE $graph->draw(('yaxis-label'=>$graphs[$i]{'yaxis-label'},'yscale'=>$graphs[$i]{'yscale'},'yaxis-max'=>$graphs[$i]{'yaxis-max'},'width'=>'640','xaxis-max'=>2051,'xaxis-line'=>1,'stroke'=>3,'strokehover'=>5,'point'=>4,'pointhover'=>6,'line'=>2,'yaxis-format'=>"commify",'yaxis-labels-baseline'=>'middle','xaxis-ticks'=>1,'left'=>$graphs[$i]{'left'}));
+
+	open(FILE,">",$dir."graphs.txt");
+	print FILE $html;
 	close(FILE);
-	
-	# Output the HTML table
-	open(FILE,'>',$dir.'graphs/'.$graphs[$i]{'table'});
-	print FILE $graph->table(());
-	close(FILE);
-	
-	$html .= "\t\t\t<figure class=\"jekyll-parse\">\n";
-	$html .= "\t\t\t\t<figcaption><strong>Figure ".($i+1).":</strong> $graphs[$i]{'title'}</figcaption>\n";
-	$html .= "\t\t\t\t<div class=\"table-holder\">{% include_relative data/graphs/$graphs[$i]{'table'} %}</div>\n";
-	$html .= "\t\t\t\t{% include_relative data/graphs/$graphs[$i]{'svg'} %}\n";
-	$html .= "\t\t\t\t<div class=\"download\">\n";
-	$html .= "\t\t\t\t\t<a href=\"data/graphs/$graphs[$i]{'svg'}\"><img src=\"resources/download.svg\" alt=\"download\" title=\"Download graph from Figure ".($i+1)."\" /> SVG</a>\n";
-	$html .= "\t\t\t\t\t<a href=\"data/graphs/$graphs[$i]{'csv'}\"><img src=\"resources/download.svg\" alt=\"download\" title=\"Download data from Figure ".($i+1)."\" /> CSV</a>\n";
-	$html .= "\t\t\t\t</div>\n";
-	$html .= "\t\t\t</figure>\n\n";
 }
-
-open(FILE,">",$dir."graphs.txt");
-print FILE $html;
-close(FILE);
-
-
 
 
 
